c5798d9749276001d38b43afc8d2f1a9d577f82c
Zer0 <igorlizaso16@gmail.com>
2022-04-10 16:53:07 +0200

    anashe

---
+++ src/commands/soporte.ts
@@ -0,0 +1,17 @@
+import Command from '../lib/structures/Command';
+import MessageEmbed from '../lib/structures/MessageEmbed';
+
+export default new Command('soporte', async (caller, cmd) => {
+	const helpEmbed = new MessageEmbed()
+		.setTitle('`Sistema de Soporte`')
+		.setDescription('`❓` ¿Qué es un Ticket?\n > Los Tickets son un método rápido y sencillo para obtener soporte por parte del Equipo de Staff, puedes preguntar dudas, comentar problemas y solicitar información de todo tipo.');
+		.setThumbnail('https://media.discordapp.net/attachments/936591912079618089/962657499046092830/a_e0b6b35c7b24c290a92c0fc22947c0d7.gif')
+		.setFooter({ text: 'Administración | Kirinuxx Stars', iconURL: 'https://media.discordapp.net/attachments/936591912079618089/962657499046092830/a_e0b6b35c7b24c290a92c0fc22947c0d7.gif' });
+	return caller.utils.discord.createMessage(cmd.channel.id, { embed: helpEmbed.code });
+},
+{
+	level: 'REGULAR',
+	aliases: []
+});
+
+
