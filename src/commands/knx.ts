56e4033dcf8aa5a630995654083f6c7af46a420d
Zer0 <igorlizaso16@gmail.com>
2022-04-12 12:38:19 +0200

    KNX rol

--- src/commands/knx.ts
+++ src/commands/knx.ts
@@ -1,7 +1,7 @@
 import Command from '../lib/structures/Command';
 import MessageEmbed from '../lib/structures/MessageEmbed';
 
-export default new Command('soporte', async (caller, cmd) => {
+export default new Command('knx', async (caller, cmd) => {
 	const helpEmbed = new MessageEmbed()
 		.setTitle('Reclamar rol KNX')
 		.setDescription('**Rol entregado <@605747883601428520>, muchas gracias por apoyar a los atuneros** <a:KNX_Verify:953370703560867911> ')
